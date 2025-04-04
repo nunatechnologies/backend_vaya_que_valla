f02e827941483c1c347fd1192af7fdeed001253d
Jair <javier.jair.cussy.saucedo@gmail.com>
2025-04-04 17:37:32 -0400

    Including roles in user resource

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use App\Enums\RolSpatie;
 
 class UserSeeder extends Seeder
 {
@@ -14,34 +15,39 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $users = [
-            [
-                'name' => 'User 1',
-                'email' => 'user1@example.com',
-                'password' => Hash::make('12345678'),
-            ],
-            [
-                'name' => 'User 2',
-                'email' => 'user2@example.com',
-                'password' => Hash::make('12345678'),
-            ],
-            [
-                'name' => 'User 3',
-                'email' => 'user3@example.com',
-                'password' => Hash::make('12345678'),
-            ],
-            [
-                'name' => 'User 4',
-                'email' => 'user4@example.com',
-                'password' => Hash::make('12345678'),
-            ],
-            [
-                'name' => 'User 5',
-                'email' => 'user5@example.com',
-                'password' => Hash::make('12345678'),
-            ]
-        ];
+        $user = User::create([
+            'name' => 'User 1',
+            'email' => 'user1@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        $user->assignRole(RolSpatie::ADMINISTRADOR->name);
 
-        User::insert($users);
+        $user = User::create([
+            'name' => 'User 2',
+            'email' => 'user2@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        $user->assignRole(RolSpatie::OPERADOR->name);
+
+        $user = User::create([
+            'name' => 'User 3',
+            'email' => 'user3@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        $user->assignRole(RolSpatie::ANUNCIANTE->name);
+
+        $user = User::create([
+            'name' => 'User 4',
+            'email' => 'user4@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        $user->assignRole(RolSpatie::AGENCIA->name);
+
+        $user = User::create([
+            'name' => 'User 5',
+            'email' => 'user5@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        $user->assignRole(RolSpatie::CLIENTE->name);
     }
 }
