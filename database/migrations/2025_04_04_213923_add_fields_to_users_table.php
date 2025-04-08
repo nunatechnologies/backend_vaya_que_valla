4176d468f2985b6c156a62cf070bb9f6d5d8f220
Jair <javier.jair.cussy.saucedo@gmail.com>
2025-04-08 14:23:54 -0400

    Saving changes in users table

---
+++ database/migrations/2025_04_04_213923_add_fields_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('type_user', ['person','organization'])->nullable()->after('password');
+            $table->string('code_phone',5)->nullable()->after('type_user');
+            $table->string('phone',15)->nullable()->after('code_phone');
+            $table->string('username',30)->nullable()->after('phone');
+            $table->string('profile_image')->nullable()->after('username');
+            $table->enum('entity_status',['active','inactive'])->default('active')->after('profile_image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['type_user','code_phone','phone','username','profile_image','entity_status']);
+        });
+    }
+};
