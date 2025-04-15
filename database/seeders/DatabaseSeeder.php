b6ea48c4c0f5a759b4b0cfb3c84542ae3459bde1
Jair <javier.jair.cussy.saucedo@gmail.com>
2025-04-15 11:40:32 -0400

    installing spatie media library

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $subDirectories = Storage::allDirectories('public');
+        foreach ($subDirectories as $value) 
+        {
+            Storage::deleteDirectory($value);
+        }
+
         $this->call([
             RolSeeder::class,
             UserSeeder::class,
