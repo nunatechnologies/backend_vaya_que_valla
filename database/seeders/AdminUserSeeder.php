df3ea438dca2abbf18d2150f2e2dfdb2b85a5a40
Jair <javier.jair.cussy.saucedo@gmail.com>
2025-04-25 23:17:32 -0400

    Added AdminUser seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Enums\RolSpatie;
+use App\Enums\UserType;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'last_name' => 'Admin',
+            'email' => 'admin@example.com',
+            'phone' => '00000000',
+            'cod_phone' => '+591',
+            'user_type' => UserType::PERSON->name,
+            'password' => Hash::make('12345678'),
+        ]);
+        $user->person()->create([
+            'ci' => '0000000',
+        ]);
+        $user->assignRole(RolSpatie::ADMINISTRADOR->name);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolSeeder::class,
             UserSeeder::class,
+            AdminUserSeeder::class,
             OrganizationSeeder::class,
             BillboardMasterSeeder::class
         ]);
