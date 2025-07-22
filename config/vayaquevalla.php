e8f1139e0513f4822c487d46c854bd3b8b7a6ccb
Jair <javier.jair.cussy.saucedo@gmail.com>
2025-07-22 16:58:56 -0400

    Added real commercial manager email

--- config/vayaquevalla.php
+++ config/vayaquevalla.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'commercial_manager_email' => 'commercialmanager@vayaquevalla.com',
+    'commercial_manager_email' => 'gerencia.comercial@vayaquevalla.com',
     'admin_url' => 'https://admin.vayaquevalla.com/',
     'panel_url' => 'https://panel.vayaquevalla.com/authentication/login/'
 ];
