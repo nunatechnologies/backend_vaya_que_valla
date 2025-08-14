6538dde56ebf255249bc28723f8184afa8f3db28
Jair <javier.jair.cussy.saucedo@gmail.com>
2025-08-14 17:27:42 -0400

    Ready to test cronjob in production

--- routes/console.php
+++ routes/console.php
@@ -4,8 +4,4 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
 Schedule::command('billboards:update-statuses')->everyMinute();
